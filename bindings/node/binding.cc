ae60fbbb1104c7c25bd9667b67d8758aeb3584e6
Isaac Azuelos <isaac@azuelos.ca>
2023-02-17 13:41:15 -0700

    Fixed capitalization in package name

--- bindings/node/binding.cc
+++ bindings/node/binding.cc
@@ -4,7 +4,7 @@
 
 using namespace v8;
 
-extern "C" TSLanguage * tree_sitter_Kurt();
+extern "C" TSLanguage * tree_sitter_kurt();
 
 namespace {
 
@@ -17,12 +17,12 @@ void Init(Local<Object> exports, Local<Object> module) {
 
   Local<Function> constructor = Nan::GetFunction(tpl).ToLocalChecked();
   Local<Object> instance = constructor->NewInstance(Nan::GetCurrentContext()).ToLocalChecked();
-  Nan::SetInternalFieldPointer(instance, 0, tree_sitter_Kurt());
+  Nan::SetInternalFieldPointer(instance, 0, tree_sitter_kurt());
 
-  Nan::Set(instance, Nan::New("name").ToLocalChecked(), Nan::New("Kurt").ToLocalChecked());
+  Nan::Set(instance, Nan::New("name").ToLocalChecked(), Nan::New("kurt").ToLocalChecked());
   Nan::Set(module, Nan::New("exports").ToLocalChecked(), instance);
 }
 
-NODE_MODULE(tree_sitter_Kurt_binding, Init)
+NODE_MODULE(tree_sitter_kurt_binding, Init)
 
 }  // namespace
